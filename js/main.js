c65ae19af1db350adedf9c0db44eeff5f055283d
MohamedCK <abdulhakimm280@gmail.com>
2023-01-30 22:36:14 +0200

    - Create a seperate function for `playerSelection`

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 const gameData = {
-    choices: ['rock', 'paper', 'scissors'],
+    choices: ["rock", "paper", "scissors"],
     roundsLeft: 5,
 };
-
 
 function computerPlay({ choices }) {
     return choices[Math.floor(Math.random() * choices.length)];
@@ -11,15 +10,31 @@ function computerPlay({ choices }) {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection === computerSelection) {
-        return "Tie!"
+        return "Tie!";
     } else if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "rock")
     ) {
-        return `You Win! ${playerSelection} beats ${computerSelection}`
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
     } else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+function playerSelection(gameData) {
+    let playerSelection = prompt(
+        `Enter your choice (rock, paper, scissors): ( You have ${gameData} rounds left).`
+    );
+    if (!playerSelection) {
+        if (confirm("Oh no you haven't finished the game, do you want to leave?")) {
+            return;
+        }
+    } else if (["rock", "paper", "scissors"].includes(playerSelection.toLowerCase().trim())) {
+        return playerSelection.toLowerCase().trim();
+    } else {
+        alert("Invalid choice. Please enter 'rock', 'paper', or 'scissors'.");
+        return playerSelection();
     }
 }
 
@@ -27,27 +42,20 @@ function game({ choices, roundsLeft }) {
     let playerScore = 0;
     let computerScore = 0;
     while (roundsLeft > 0) {
-        let playerSelection = prompt(`Enter your choice (rock, paper, scissors): ( You have ${roundsLeft} rounds left).`);
-        if (!playerSelection) {
-            if (confirm("Oh no you haven't finished the game, do you want to leave?")) {
-                return;
-            }
-        } else if (["rock", "paper", "scissors"].includes(playerSelection.toLowerCase().trim(" "))) {
-            let computerSelection = computerPlay({ choices });
-            let result = playRound(playerSelection, computerSelection);
-            if (result.startsWith("You Win")) {
-                playerScore++;
-                alert(`Congrats! ${result}\n Click ok to go to the next round.`);
-            } else if (result.startsWith("You Lose")) {
-                computerScore++;
-                alert(`Sorry! ${result}\n Click ok to go to the next round.`);
-            } else {
-                alert(`It's a ${result}\n Click ok to go to the next round.`);
-            }
-            roundsLeft--;
+        let player = playerSelection(roundsLeft);
+        if (!player) return;
+        let computer = computerPlay({ choices });
+        let result = playRound(player, computer);
+        if (result.startsWith("You Win")) {
+            playerScore++;
+            alert(`Congrats! ${result}\n Click ok to go to the next round.`);
+        } else if (result.startsWith("You Lose")) {
+            computerScore++;
+            alert(`Sorry! ${result}\n Click ok to go to the next round.`);
         } else {
-            alert("Invalid choice. Please enter 'rock', 'paper', or 'scissors'.")
+            alert(`It's a ${result}\n Click ok to go to the next round.`);
         }
+        roundsLeft--;
     }
     displayWinner(playerScore, computerScore);
 }
