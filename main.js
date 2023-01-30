0ba3b9c9c667328a69c64b215f87551fd0200263
MohamedCK <abdulhakimm280@gmail.com>
2023-01-30 10:37:50 +0200

    - Include the game logic

--- main.js
+++ main.js
@@ -22,3 +22,35 @@ function playRound(playerSelection, computerSelection) {
         return `You Lose! ${computerSelection} beats ${playerSelection}`
     }
 }
+
+function game({ choices, roundsLeft }) {
+    let playerScore = 0;
+    let computerScore = 0;
+    while (roundsLeft > 0) {
+        let playerSelection = prompt("Enter your choice (rock, paper, scissors): ");
+        if (!["rock", "paper", "scissors"].includes(playerSelection.toLowerCase())) {
+            alert("Invalid choice. Please enter 'rock', 'paper', or 'scissors'.")
+            continue;
+        }
+        let computerSelection = computerPlay({ choices })
+        let result = playRound(playerSelection, computerSelection);
+        if (result.startsWith("You Win")) {
+            playerScore++;
+            alert(`Congrats! ${result} you have ${roundsLeft} rounds left.`);
+        } else if (result.startsWith("You Lose")) {
+            computerScore++;
+            alert(`Sorry! ${result} you have ${roundsLeft} rounds left.`);
+        } else {
+            alert(`It's a ${result} you have ${roundsLeft} rounds left.`);
+        }
+        roundsLeft--;
+    }
+    if (playerScore > computerScore) {
+        alert("Congratulations! You won the game.\nFinal Score: You " + playerScore + " - Computer " + computerScore);
+    } else if (playerScore < computerScore) {
+        alert("Sorry! You lost the game.\nFinal Score: You " + playerScore + " - Computer " + computerScore);
+    } else {
+        alert("It's a tie!\nFinal Score: You " + playerScore + " - Computer " + computerScore);
+    }
+}
+
