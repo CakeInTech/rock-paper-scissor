151722ee01e0e3dd1d0574b5a47c5c4b0f3e74a8
MohamedCK <abdulhakimm280@gmail.com>
2023-01-30 10:09:45 +0200

    - Create a function that generates computer choice

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ const gameData = {
     choices: ['rock', 'paper', 'scissors'],
     roundsLeft: 5,
 };
+
+
+function computerPlay({ choices }) {
+    return choices[Math.floor(Math.random() * choices.length)];
+}
