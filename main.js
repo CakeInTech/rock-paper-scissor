2b870452872d50122cf2933e98b66abb352f7bbd
MohamedCK <abdulhakimm280@gmail.com>
2023-01-30 10:19:52 +0200

    - Create a function to determine who is the winner

--- main.js
+++ main.js
@@ -7,3 +7,18 @@ const gameData = {
 function computerPlay({ choices }) {
     return choices[Math.floor(Math.random() * choices.length)];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "Tie!"
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")
+    ) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
